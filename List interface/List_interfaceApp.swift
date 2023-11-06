1d213c8ea385cc05264d765a524b81efd00e9b5c
Pablo Alvarado <palvarado@lcs.on.ca>
2023-11-06 14:33:38 -0500

    Initial Commit

---
+++ List interface/List_interfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  List_interfaceApp.swift
+//  List interface
+//
+//  Created by Pablo Alvarado on 06/11/23.
+//
+
+import SwiftUI
+
+@main
+struct List_interfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
