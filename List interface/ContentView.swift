37ac8841fc0f1db2697c5aeda797699bdcdf5a83
Pablo Alvarado <palvarado@lcs.on.ca>
2023-11-06 20:46:05 -0500

    Got stuck here. Not sure how to debug but I already have a structure for TaskList.

--- List interface/ContentView.swift
+++ List interface/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack(spacing: 20) {
+            }
+            .navigationTitle("To do")
+            .font(.title)
+            .toolbar {
+                HStack{
+                    ToolbarItem (placement: .topBarLeading){
+                        Button(action: {}) {
+                            Image(systemName: "arrow.left")
+                                .resizable()
+                                .foregroundColor(.blue)
+                        }
+                    }
+                    Text("Lists")
+                        .foregroundColor(.blue)
+                }
+                ToolbarItem(placement: .topBarTrailing)
+                Button(action: {}) {
+                    Image(systemName: "square.and.arrow.up")
+                        .resizable()
+                        .foregroundColor(.blue)
+                }
+                Spacer()
+               
+            }
+            .tint(.blue)
         }
-        .padding()
     }
 }
 
